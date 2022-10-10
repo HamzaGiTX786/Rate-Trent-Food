596b538a34fe81010dd8e0f812062c2fdee5c8bf
Hamza <attarwalahamza@gmail.com>
2022-10-10 17:10:06 -0400

    no result

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ $errors = array(); //declare empty array to add errors too
 
 if(isset($_POST['submit']))
 {
-    if (!isset($search) || strlen($search) === 0) 
+    if (!isset($search) || strlen($search) === 0) // check if user entered anything into the search bar
     {
         $errors['search'] = true;
     }
 
-    if(count($errors) === 0)
+    if(count($errors) === 0) //check if there are any errors
     {
         $searchquery = "%$search%";
         $query = "SELECT itemname FROM fooditems WHERE itemname LIKE ? ";
@@ -25,7 +25,7 @@ if(isset($_POST['submit']))
             mysqli_stmt_bind_param($stmt,"s",$searchquery);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
-            $food = mysqli_fetch_assoc($result);
+            $food = mysqli_fetch_assoc($result); // get output for the searched item
     }
 }
 }
@@ -51,19 +51,25 @@ if(isset($_POST['submit']))
        
     <p>Welcome to Rate Trent Food. This a website where you can give a rating to a dish served in any of Trent University's Cafes from 1-5</p>
     <form name="search" id="search" method="post" novalidate>
+
+        <!-- Search bar -->
     <div>
         <label for="search">Search</label>
         <input type="text" name="search" id="search" placeholder="Search" />
         <span class="error <?=!isset($errors['search']) ? 'hidden' : "";?>">Please enter item to be searched!</span>
     </div>
 
+    <!-- Button to submit -->
+
     <div id="buttons">
     <button type="submit" name="submit">Search</button>
     </div>
-
     </form>
 
-    <div id="searchresult" class = "<?=!isset($food) ? 'hidden':"";?>">
+    <!-- Search Results -->
+
+    <!-- The result section is hidden until there is aything found -->
+    <div id="searchresult" class = "<?=!isset($food) ? 'hidden':"";?>"> 
     <p>Search Result: </p>
     <?php foreach($food as $fooditem):?>
         <ul>
@@ -71,6 +77,12 @@ if(isset($_POST['submit']))
         </ul>
     <?php endforeach; ?>
     </div>
+
+    <?php if(!isset($food)){?>
+        <div>
+        <p>Search Result: No results found </p>
+        </div>
+    <?php } ?>
     
     <?php
         include 'includes/footer.php';

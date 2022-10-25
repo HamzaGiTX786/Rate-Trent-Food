b8aceaf547784ab00ab68a1b1b38a1157d7e76e0
raychen72 <77996690+raychen72@users.noreply.github.com>
2022-10-25 11:34:43 -0400

    Update menuandcafe.php

--- menuandcafe.php
+++ menuandcafe.php
@@ -46,7 +46,7 @@ if(isset($_POST['submit']))
  
 <?php
         include 'includes/header.php';
-        include 'includes/nav.php';
+        include 'includes/navfood.php';
  ?>
     <main>
     <p>Welcome to Rate Trent Food. This a website where you can give a rating to a dish served in any of Trent University's Cafes from 1-5</p>

2de03750cab4e8910bf927bf2426857a1e9685aa
raychen72 <77996690+raychen72@users.noreply.github.com>
2022-10-25 11:34:10 -0400

    Create navfood.php

---
+++ includes/navfood.php
@@ -0,0 +1,9 @@
+<nav>
+            <ul>
+                <a href="Champlain.php"><li>Champlain</li></a>
+                <a href="Gzowski.php"><li>Gzowski</li></a>
+                <a href="LadyEaton.php"><li>Lady Eaton</li></a>
+                <a href="Otonabee.php"><li>Otonabee</li></a>
+                
+            </ul>
+</nav>

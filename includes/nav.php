b55af19236d7e6b2504f62d406c9aefa7e298c78
Hamza A <hamzasaliattarwala@trentu.ca>
2022-11-22 12:21:32 -0500

    minor changes

--- includes/nav.php
+++ includes/nav.php
@@ -3,7 +3,7 @@
                 <a href="login.php"><li>Login</li></a>
                 <a href="createaccount.php"><li>Create Account</li></a>
                 <a href="menuandcafe.php"><li>Menu & Cafes</li></a>
-                <a href="backend.php"><li>Backend Access</li></a>
+                <a href="backendaccess.php"><li>Backend Access</li></a>
                 <a href="RateaDish.php"><li>Rate a Dish</li></a>
             </ul>
 </nav>
